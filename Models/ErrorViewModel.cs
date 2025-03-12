4fcf5d59413a913e86f62cd9fd0f211aa20f9ff2
NiyatiAgravat-1409 <nagravat066@rku.ac.in>
2025-03-12 18:00:08 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZUVO___CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
